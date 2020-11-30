dbaf7edf3f2dc59650207ef79a3e8d9a8e5ba27c
ZangTiankai <907609440@qq.com>
2020-12-01 00:06:13 +0800

    Modified on 12.1
    Some improvement on basic UI.
    Some unnecessary .png were removed.
    The name one part of Bar_1 was changed.

--- Wanderlust_0/app/src/main/java/com/example/wanderlust_0/Bar_1_Map.java
+++ Wanderlust_0/app/src/main/java/com/example/wanderlust_0/Bar_1_Map.java
@@ -12,8 +12,8 @@ import androidx.fragment.app.Fragment;
 public class Bar_1_Map extends Fragment {
 
 
-    Bar_1_Control c;
-    Bar_1_Map(Bar_1_Control c){
+    Bar_1_Selector c;
+    Bar_1_Map(Bar_1_Selector c){
         super();
         this.c=c;
     }
